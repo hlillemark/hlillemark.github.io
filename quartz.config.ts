41848844d78dc380bd5c792bbb553493f8ecd1fe
hlillemark <hlillemark@gmail.com>
2025-06-25 00:41:23 -0400

    Quartz sync: Jun 25, 2025, 12:41 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "hlillemark.github.io",
+    baseUrl: "hlillemark.github.io/obsidian-website",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
